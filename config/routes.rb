340a4bd5e62003a8030eece687febeac2d6c3711
john tremont <piloto2810@gmail.com>
2021-08-18 23:45:40 -0400

    added some styles and refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
+  resources :tasks
   # index
-  get '/tasks', to: 'tasks#index'
+  # get '/tasks', to: 'tasks#index'
   # new
-  get '/tasks/new', to: 'tasks#new', as: :new_task
+  # get '/tasks/new', to: 'tasks#new', as: :new_task
   # create
-  post 'tasks', to: 'tasks#create'
+  # post 'tasks', to: 'tasks#create'
   # show
-  get '/tasks/:id', to: 'tasks#show', as: :task
+  # get '/tasks/:id', to: 'tasks#show', as: :task
   # edit
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   # update
-  patch 'tasks/:id', to: 'tasks#update'
-  #delete
-  delete "tasks/:id",      to: "tasks#destroy"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete
+  # delete "tasks/:id",      to: "tasks#destroy"
 end
